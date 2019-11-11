ef9c951e7e64eb83fb3136cca40248d081b6f701
Ashok Thakur <iashok180@gmail.com>
2019-11-11 14:13:12 +0530

    EDA added

--- test.py
+++ test.py
@@ -14,19 +14,18 @@ def days_count(date):
 
 
 def weekdays(days):
-    weekdays = (days+3) % 7  # star date is wed
-    if weekdays == 0:
-        weekdays = 7
-    return weekdays
+    weekday_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    weekday = (days+3) % 7  # star date is wed
+    if weekday == 0:
+        weekday = 7
+    return weekday_names[weekday-1]
 
 
 # Date Processing
 df['Date'] = pd.to_datetime(df['Date'])
 df['Days'] = df['Date'].apply(days_count).astype(int)
-df['Week_key'] = df['Days'].apply(weekdays)
-df['Weekend'] = df['Week_key'].apply(lambda x: 1 if x in [1, 7] else 0)
-
-print(df.head())
+df['Weekdays'] = df['Days'].apply(weekdays)
+df['Weekend'] = df['Weekdays'].apply(lambda x: 1 if x in ['Saturday', 'Sunday'] else 0)
 
 # Scaling
 df['Ad_impression_thousands'] = round(df['Ad_impression']/1000).astype(int)
@@ -37,16 +36,30 @@ ax = fig.add_subplot(111)
 ax = sns.lineplot(df['Days'], df['Viewership'])
 plt.show()
 plt.cla()
+
 # Viewerships are good on most of the weekends
-ax = sns.scatterplot(x='Days', y='Viewership', data=df, hue='Weekend')
+ax = sns.boxplot(x='Weekdays', y='Viewership', data=df)
 plt.show()
 plt.cla()
-# Viewership has picked up in presence of a character but again fall down in its absence
-ax = sns.scatterplot(x='Days', y='Viewership', data=df, hue='CharacterA')
+
+# Viewership has picked up in presence of a character but again fell down in its absence
+ax = sns.stripplot(x='CharacterA', y='Viewership', data=df, hue='Weekdays', jitter=True)
+plt.show()
+plt.cla()
+
+# Viewership has picked up even on weekends in presence of a character
+ax = sns.boxplot(x='Weekdays', y='Viewership', data=df, hue='CharacterA')
+plt.show()
+plt.cla()
+
+# There is no such impact of a cricket match at any day
+ax = sns.scatterplot(x="Days", y="Viewership", hue="Cricket_match", data=df)
 plt.xlabel('Days')
 plt.ylabel('Viewership')
 plt.legend()
 plt.show()
+
+
 # Weekend and Ad Impression are correlated with Viewership
 ax = sns.heatmap(df.corr(), annot=True)
 plt.show()
